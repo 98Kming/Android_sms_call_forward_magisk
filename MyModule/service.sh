e628b9dcd1d284ee01818280e38170f11a8fd8ab
kming <1450030253@qq.com>
2025-04-16 17:07:26 +0800

    手动脚本不显示输出日志

--- MyModule/service.sh
+++ MyModule/service.sh
@@ -42,6 +42,6 @@ cleanup() {
 trap cleanup EXIT
 
 #/data/adb/ksu/bin/busybox sh -o standalone script -q -c "$MODDIR/test.sh" /dev/null | awk -v ts="$timestamp" 'BEGIN{RS="^$"} {printf "[%s] %s\n", ts, $0; fflush()}' | tee -a $MODDIR/log.log
-logSh "$MODDIR/forward.sh"
+logSh "$MODDIR/forward.sh" $1
 #/system/bin/sh $MODDIR/test.sh 2>&1 2>&1 | awk -v ts="$timestamp" 'BEGIN{RS="^$"} {printf "[%s] %s\n", ts, $0}' | tee $MODDIR/log.log
 
